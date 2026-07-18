2545de3b9961dee87fff92a639a6e038a8ef8a54
Synora Deploy <toantran30052003-oss@users.noreply.github.com>
2026-07-18 19:09:08 +0700

    feat: launch Synora landing and content studio

---
+++ _next/static/dcHwQsAFVK7ZxKf3CXiW_/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/synora-ai-marketing-//_next/:path+",
+        "destination": "/synora-ai-marketing-/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
